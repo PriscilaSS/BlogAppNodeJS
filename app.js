d9d802e0147cbc179801d146282cc67029930c27
PriscilaSS <priscilas_silva@yahoo.com.br>
2021-01-29 14:13:00 -0300

    Adicionar gitignore e variáveis ambiente

--- app.js
+++ app.js
@@ -15,6 +15,7 @@
     const usuarios = require("./routes/usuario")
     const passport = require("passport")
     require("./config/auth")(passport)
+    const db = require("./config/db")
 
 // Configurações
     // Sessão
@@ -47,7 +48,7 @@
         app.set('view engine', 'handlebars')
     // Mongoose
         mongoose.Promise = global.Promise
-        mongoose.connect("mongodb://localhost/teste-node-blogapp").then(() => {
+        mongoose.connect(db.mongoURI).then(() => {
             console.log("Conectado ao mongo")
         }).catch((err) => {
             console.log("Erro ao se conectar: " + err)
@@ -120,7 +121,7 @@
     app.use("/usuarios", usuarios)
 
 // Outros
-    const PORT = 8081
+    const PORT = process.env.PORT || 8081
     app.listen(PORT, () => {
         console.log("Servidor rodando!")
     })
